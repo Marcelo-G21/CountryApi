8b9890a9381b68b4e6afa95f6c1e14eda27509d8
Marcelo García <99058282+Marcelo-G21@users.noreply.github.com>
2022-11-22 16:17:27 -0300

    Add files via upload

---
+++ js/detail.js
@@ -0,0 +1,58 @@
+const root = document.getElementById('root');
+
+const query = new URLSearchParams(window.location.search);
+const params = query.get('name');
+const url = "https://restcountries.com/v3.1/all";
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+});
+
+const fetchData = async () => {
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+      
+        const filterData = data.filter(item => item.name.common === params);
+      
+        countryDetail(filterData);
+    }catch (error){
+        console.log(error);
+    }
+  
+}
+
+const countryDetail = data => {
+    let elements = ''
+
+    function getValues(obj){
+        if (obj === undefined){
+            return 'Undefined'
+        }
+        return Object.values(obj).join(', ');
+    }
+
+    
+
+    data.forEach(item => {
+        elements += `
+        <div class="card">
+          <div class="card-body">
+          <img class="flag" src="${item.flags.png}" alt="" />
+          <img class="coat-of-arms" src="${item.coatOfArms.png}" alt="" />
+            <div class="card-description">
+              <h2>${item.name.common}</h2>
+              <p>Official name: ${item.name.official}</p>
+              <p>Region: ${item.region}</p>
+              <p>Capital: ${getValues(item.capital)}</p>
+              <p>Languages: ${getValues(item.languages)}</p>
+              <p>Population: ${item.population}</p>
+              <p>Time Zone: ${getValues(item.timezones)}</p>
+              <p>Borders: ${getValues(item.borders)}</p>
+            </div>
+          </div>
+        </div>
+        `
+    });
+    root.innerHTML = elements;
+}
